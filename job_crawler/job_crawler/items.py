ab9ebcaf484b82aa60ef2add837a25ad91873e50
Aesop <hsu00093@gmail.com>
2026-02-15 23:19:37 +0800

    update: 開始寫爬蟲

--- job_crawler/job_crawler/items.py
+++ job_crawler/job_crawler/items.py
@@ -8,66 +8,29 @@ import scrapy
 
 class JobCrawlerItem(scrapy.Item):
     # name = scrapy.Field()
-    
-    # jobs
-    # 104的工作代號
+
     job_code = scrapy.Field()
-    # 職位名稱
-    job_title = scrapy.Field()
-    # 縣市
-    cnc = scrapy.Field()
-    # 地點
-    location = scrapy.Field()
-    # 歡迎各類人
-    special = scrapy.Field()
-    # --條件要求
-    # 工作經驗
-    work_exper = scrapy.Field()
-    # 學歷要求
-    educational = scrapy.Field()
-    # 科系要求
-    d_requirements = scrapy.Field()
-    # 語文條件
-    language = scrapy.Field()
-    # 工作技能
-    workskill = scrapy.Field()
-    # 其他條件
-    other = scrapy.Field()
-    # 薪水區間
-    salary_min = scrapy.Field()
-    salary_max = scrapy.Field()
-    # 面議
-    negotiable = scrapy.Field()
-    # 職位連結
-    link = scrapy.Field()
+    raw_data = scrapy.Field()
+    category_code = scrapy.Field()
     
-    # company
-    # 公司
-    company = scrapy.Field()
-    
-    # job_stats
-    # 更新時間
-    update_date = scrapy.Field()
-    # 應徵人數
-    apply_num = scrapy.Field()
-    
-    # skill
-    # 技能
-    skills = scrapy.Field()
-    # 擅長工具 good at tools
-    gat = scrapy.Field()
-    
-    # rules
-    # 出差
-    obt = scrapy.Field()
-    # 上班時段
-    job_time = scrapy.Field()
-    # 休假制度
-    vaction = scrapy.Field()
-    
-    # welfare
-    # --待遇
-    # 福利
-    bonn = scrapy.Field()
-    # 福利自介(會從裡面萃取精華出來)
-    bonn_text = scrapy.Field()
+    # company_name = scrapy.Field()
+
+    # job_code = scrapy.Field()
+    # snapshot_date = scrapy.Field()
+    # job_title = scrapy.Field()
+    # location = scrapy.Field()
+    # apply_num = scrapy.Field()
+    # salary = scrapy.Field()
+    # negotiable = scrapy.Field()
+    # yearbonus = scrapy.Field()
+    # job_released = scrapy.Field()
+    # obt = scrapy.Field()
+    # no_exper = scrapy.Field()
+    # remote = scrapy.Field()
+
+    # job_time = scrapy.Field()
+    # vacation = scrapy.Field()
+    # link = scrapy.Field()
+    # skill_list = scrapy.Field()
+    # benefit_list = scrapy.Field()
+    # special = scrapy.Field()
--- job_crawler/job_crawler/spiders/job.py
+++ job_crawler/job_crawler/spiders/job.py
@@ -1,10 +1,12 @@
 import scrapy
-
+import json
+import mysql.connector
+from dotenv import load_dotenv
+load_dotenv()
 
 class Job_marketSpider(scrapy.Spider):
     name = "job"
     allowed_domains = ["104.com.tw"]
-    start_urls = ["https://104.com.tw"]
-
-    def parse(self, response):
+    start_urls = [f"https://www.104.com.tw/jobs/search/?jobsource=joblist_search&mode=s&order=16&page=1&area={}&jobcat={}"]
+    def parse(self):
         
