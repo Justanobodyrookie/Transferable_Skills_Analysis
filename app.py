62b25c86953717f6c2bf007599eac4d6a4d7a0a4
Aesop <hsu00093@gmail.com>
2026-03-10 16:49:01 +0800

    update: 帳號密碼系統完成，使用bcrypt套件

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import mysql.connector
 import os
 import random
 import json
+import bcrypt
 import plotly.express as px
 from dotenv import load_dotenv
 load_dotenv()
+st.set_page_config(layout="wide", page_title="轉職嗎?")
 db_config = {
   'host': os.getenv('MYSQL_HOST'),
   'port': os.getenv('MYSQL_PORT'),
@@ -14,6 +16,10 @@ db_config = {
   'password': os.getenv('MYSQL_ROOT_PASSWORD'),
   'database': os.getenv('MYSQL_DATABASE')
 }
+def hash_password(password):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+def verify_password(password, hash_password):
+    return bcrypt.checkpw(password.encode('utf-8'), hash_password.encode('utf-8'))
 @st.cache_data(ttl=3600)
 def load_data(query, params=None):
     conn = mysql.connector.connect(**db_config)
@@ -38,8 +44,41 @@ def db_search(query, params=None):
     conn.close()
     options = ["請選擇"] + df.iloc[:, 0].tolist()
     return options
-
-page = st.sidebar.radio("系統功能", ['職務技能需求表', '技能適配度檢測'])
+def execute_query(query, params=None):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query, params)
+        conn.commit()
+        return True
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return False
+    finally:
+        cursor.close()
+        conn.close()
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+if 'username' not in st.session_state:
+    st.session_state['username'] = None
+if st.session_state['logged_in']:
+    menu_options = ['職務技能需求表', '技能適配度檢測', '職缺閱覽']
+else:
+    menu_options = ['職務技能需求表', '技能適配度檢測', '系統登入']
+if st.session_state['logged_in']:
+    st.sidebar.markdown('---')
+    st.sidebar.write(f"管理員: **{st.session_state['username']}**")
+    if st.sidebar.button('登出'):
+        st.session_state['logged_in'] = False
+        st.session_state['username'] = None
+        st.rerun()
+page = st.sidebar.radio("系統功能", menu_options)
+st.sidebar.markdown("---")
+st.sidebar.markdown("**Contact:** hsu00093@gmail.com") # 換成你的真實信箱
+st.sidebar.markdown(
+    "<span style='font-size: 0.8em; color: gray;'>本系統僅供個人興趣專案，爬取之資料不做任何商業用途。</span>", 
+    unsafe_allow_html=True
+)
 # SQL區
 ind_df = load_data("select ind_name from industries where ind_name is not null;")
 ind_list = ind_df['ind_name'].tolist()
@@ -200,58 +239,57 @@ elif page == '技能適配度檢測':
         st.write('---')
         st.write(f"您的技能包: {select_small}")
         st.write(f"您的語言能力: {select_langs} ({user_lang_levels})")
-        level_accept_map = {
-            "不拘": [0],
-            "略懂": [0, 4],
-            "中等": [0, 4, 8],
-            "精通": [0, 4, 8, 2]
-        }
-        lang_conditions = []
-        lang_params = []
+        dynamic_joins = []
         final_params = []
-        defense = ', '.join(['%s'] * len(select_small))
-        final_params.extend(select_small)
-        for lang, level_text in user_lang_levels.items():
-            acc_code = level_accept_map[level_text]
-            defense_lang = ', '.join(['%s'] * len(acc_code))
-            step1 = f"""(
-                l.name = %s 
-                and jl.speak in ({defense_lang})
-                and jl.listen in ({defense_lang})
-                and jl.reading in ({defense_lang})
-                and jl.writing in ({defense_lang})
-            )"""
-            lang_conditions.append(step1)
-            lang_params.append(lang)
-            lang_params.extend(acc_code)
-            lang_params.extend(acc_code)
-            lang_params.extend(acc_code)
-            lang_params.extend(acc_code)
-        final_lang_sql = " or ".join(lang_conditions)
-        final_params.extend(lang_params)
+        if select_small:
+            defense = ', '.join(['%s'] * len(select_small))
+            skill_join = f"""
+                join(
+                    select js.job_id
+                    from job_skills js
+                    join skills s on js.skills_id = s.id
+                    where s.name in ({defense})
+                    group by js.job_id
+                    having count(distinct s.name) >= 1
+                ) as mj on j.id = mj.job_id
+            """
+            dynamic_joins.append(skill_join)
+            final_params.extend(select_small)
+        if select_langs:
+            level_accept_map = {"不拘": [0], "略懂": [0, 4], "中等": [0, 4, 8], "精通": [0, 4, 8, 2]}
+            lang_conditions = []
+            for lang, level_text in user_lang_levels.items():
+                acc_code = level_accept_map[level_text]
+                defense_lang = ', '.join(['%s'] * len(acc_code))
+                step1 = f"""(
+                    l.name = %s
+                    and jl.speak in ({defense_lang})
+                    and jl.listen in ({defense_lang})
+                    and jl.writing in ({defense_lang})
+                    and jl.reading in ({defense_lang})
+                )"""
+                lang_conditions.append(step1)
+                final_params.append(lang)
+                final_params.extend(acc_code * 4)
+            final_lang_sql = " or ".join(lang_conditions)
+            lang_join = f"""
+                join(
+                    select jl.job_id
+                    from job_languages jl
+                    join languages l on jl.language_id = l.id
+                    where {final_lang_sql}
+                    group by jl.job_id
+                    having count(l.name) >= 1
+                ) as ml on j.id = ml.job_id
+            """
+            dynamic_joins.append(lang_join)
+        join_string = "\n".join(dynamic_joins)
         final_sql = f"""
-            with match_jobs as (
-                select j.id, j.no_exper
-                from jobs j
-                join job_skills js on j.id = js.job_id
-                join skills s on js.skills_id = s.id
-                where s.name in ({defense})
-                group by j.id, j.no_exper
-                having count(distinct s.name) >= 1
-                ),
-            match_langs as (
-                select jl.job_id
-                from job_languages jl
-                join languages l on jl.language_id = l.id
-                where {final_lang_sql}
-                group by jl.job_id
-                having count(l.name) >= 1
-            )
-                select
-                    count(mj.id) as 總職缺數,
-                    sum(mj.no_exper) as 無經驗可
-                from match_jobs mj
-                join match_langs ml on mj.id = ml.job_id;
+            select
+                count(j.id) as 總職缺數,
+                sum(j.no_exper) as 無經驗可
+            from jobs j
+            {join_string};
         """
         result_df = load_data(final_sql, tuple(final_params))
         total_jobs = int(result_df['總職缺數'].iloc[0])
@@ -264,31 +302,13 @@ elif page == '技能適配度檢測':
         st.write('### 職缺市場機會')
         # 薪水地帶
         salary_sql = f"""
-            with match_jobs as (
-                select j.id, j.no_exper
-                from jobs j
-                join job_skills js on j.id = js.job_id
-                join skills s on js.skills_id = s.id
-                where s.name in ({defense})
-                group by j.id, j.no_exper
-                having count(distinct s.name) >= 1
-                ),
-            match_langs as (
-                select jl.job_id
-                from job_languages jl
-                join languages l on jl.language_id = l.id
-                where {final_lang_sql}
-                group by jl.job_id
-                having count(l.name) >= 1
-            )
             select
                 j.salary_type,
                 count(j.id) as 職缺數,
                 round(avg(case when j.salary_min > 0 then j.salary_min else null end), 0) as 平均底薪,
                 round(avg(case when j.salary_max > 0 then j.salary_max else null end), 0) as 平均天花板
-            from match_jobs mj
-            join match_langs ml on mj.id = ml.job_id
-            join jobs j on mj.id = j.id
+            from jobs j
+            {join_string}
             group by j.salary_type;
         """
         m1, m2 = st.columns(2)
@@ -321,8 +341,7 @@ elif page == '技能適配度檢測':
                 piecework_data = salary_df[salary_df['salary_type'] == 2]
                 if not piecework_data.empty:
                     m_min = int(piecework_data['平均底薪'].iloc[0])
-                    m_max = int(piecework_data['平均天花板'].iloc[0])
-                    st.metric(label='論件計酬', value=f"{m_min} ~ {m_max}")
+                    st.metric(label='論件計酬', value=f"{m_min}")
                 part_time_data = salary_df[salary_df['salary_type'] == 3]
                 if not part_time_data.empty:
                     m_min = int(part_time_data['平均底薪'].iloc[0])
@@ -330,29 +349,12 @@ elif page == '技能適配度檢測':
             st.write('---')
             st.write('### 適合的職務方向')
             rank_sql = f"""
-                with match_jobs as (
-                    select j.id, j.no_exper
-                    from jobs j
-                    join job_skills js on j.id = js.job_id
-                    join skills s on js.skills_id = s.id
-                    where s.name in ({defense})
-                    group by j.id, j.no_exper
-                    having count(distinct s.name) >= 1
-                    ),
-                match_langs as (
-                    select jl.job_id
-                    from job_languages jl
-                    join languages l on jl.language_id = l.id
-                    where {final_lang_sql}
-                    group by jl.job_id
-                    having count(l.name) >= 1
-                )
                 select
                     jc.name as 適合職缺,
-                    count(mj.id) as 匹配數量
-                from match_jobs mj
-                join match_langs ml on mj.id = ml.job_id
-                join job_category_relations jcr on mj.id = jcr.job_id
+                    count(j.id) as 匹配數量
+                from jobs j
+                {join_string}
+                join job_category_relations jcr on j.id = jcr.job_id
                 join job_category jc on jcr.category_code = jc.code
                 group by jc.name
                 order by 匹配數量 desc
@@ -364,3 +366,154 @@ elif page == '技能適配度檢測':
                 st.dataframe(rank_df, hide_index=True)
             else:
                 st.info("目前沒有足夠的資料分析職缺")
+elif page == '系統登入':
+    st.title('系統後台登入')
+    auth_mode = st.radio('請選擇操作', ['登入', '註冊新帳號'], horizontal=True)
+    if auth_mode == '登入':
+        login_user = st.text_input('帳號')
+        login_password = st.text_input('密碼', type='password')
+        if st.button('登入'):
+            if login_user and login_password:
+                check_sql = "select password_hash from users where username = %s"
+                user_data = load_data(check_sql, (login_user,))
+                if not user_data.empty:
+                    db_hash = user_data['password_hash'].iloc[0]
+                    if verify_password(login_password, db_hash):
+                        st.session_state['logged_in'] = True
+                        st.session_state['username'] = login_user
+                        st.success('登入成功')
+                        st.rerun()
+                    else:
+                        st.error('密碼錯誤')
+                else:
+                    st.error('帳號錯誤')
+            else:
+                st.warning('請輸入帳號與密碼')
+    elif auth_mode == '註冊新帳號':
+        reg_user = st.text_input('設定帳號')
+        reg_password = st.text_input('設定密碼')
+        sc = st.text_input('通關密碼', type='password')
+        if st.button('確認註冊'):
+            if not reg_user or not reg_password or not sc:
+                st.warning('所有欄位皆為必填')
+            elif sc != os.getenv('SC'):
+                st.error('通關密碼錯誤, 繼續猜吧')
+            else:
+                check_exist = load_data('select id from users where username = %s', (reg_user,))
+                if not check_exist.empty:
+                    st.error('此帳號已被使用')
+                else:
+                    hashed_password = hash_password(reg_password)
+                    insert_sql = 'insert into users (username, password_hash) values (%s, %s)'
+                    if execute_query(insert_sql, (reg_user, hashed_password)):
+                        st.success('註冊成功, 請切換至登入頁面')
+                    else:
+                        st.error('系統發生錯誤')
+elif page == '職缺閱覽':
+    st.title('歡迎來到地下寶庫')
+    my_quote = quote('words.json')
+    st.markdown(f"> *{my_quote}*")
+    st.write('### 快速過濾')
+    benefits_sql = 'select name from benefits'
+    benefits_df = load_data(benefits_sql)
+    benefits_list = benefits_df['name'].tolist()
+    region_sql = 'select name from regions where level = 2'
+    region_df = load_data(region_sql)
+    region_list = region_df['name'].tolist()
+    st.write('---')
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        search_keyword = st.text_input('關鍵字搜尋 (職缺或公司名稱)')
+    with col2:
+        select_benefits = st.multiselect('要求福利', benefits_list)
+    with col3:
+        select_region = st.multiselect('縣市', region_list)
+    with col4:
+        only_no_exper = st.checkbox('無經驗可')
+    dynamic_vip_joins = []
+    vip_params = []
+    if select_benefits:
+        defense_bene = ', '.join(['%s'] * len(select_benefits))
+        bene_sql = f"""
+            join(
+                select jb.job_id
+                from job_benefits jb
+                join benefits b on jb.be_code = b.be_code
+                where b.name in ({defense_bene})
+                group by jb.job_id
+                having count(distinct b.name) = {len(select_benefits)}
+            ) as match_be on j.id = match_be.job_id
+         """
+        dynamic_vip_joins.append(bene_sql)
+        vip_params.extend(select_benefits)
+    if select_region:
+        protect_region = ', '.join(['%s'] * len(select_region))
+        reg_sql = f"""
+            join(
+                select code from regions where name in ({protect_region})
+                union
+                select r2.code from regions r1
+                join regions r2 on r1.code = r2.parent_code
+                where r1.name in ({protect_region})
+            ) as match_reg on j.region_code = match_reg.code
+        """
+        dynamic_vip_joins.append(reg_sql)
+        vip_params.extend(select_region)
+        vip_params.extend(select_region)
+    vip_join_string = '\n'.join(dynamic_vip_joins)
+    vip_sql = f"""
+        select
+            r.name as 地區,
+            c.name as 公司名稱,
+            j.job_title as 職缺名稱,
+            round(j.response_pr * 100, 0) as hr回覆率,
+            case j.salary_type
+                when 1 then '面議'
+                when 2 then '論件'
+                when 3 then '時薪'
+                when 4 then '日薪'
+                when 5 then '月薪'
+                when 6 then '年薪'
+                else '未知'
+            end as 支薪方式,
+            j.salary_min as 最低薪資,
+            j.salary_max as 最高薪資,
+            group_concat(distinct s.name separator ', ') as 技能要求,
+            group_concat(distinct b.name separator ', ') as 公司福利,
+            j.link as 職缺連結
+        from jobs j
+        {vip_join_string}
+        join company c on j.company_id = c.id
+        left join regions r on j.region_code = r.code
+        left join job_skills js on j.id = js.job_id
+        left join skills s on js.skills_id = s.id
+        left join job_benefits jb2 on j.id = jb2.job_id
+        left join benefits b on jb2.be_code = b.be_code
+        where 1 = 1
+    """
+    if search_keyword:
+        vip_sql = vip_sql + " and (j.job_title like %s or c.name like %s)"
+        vip_params.extend([f"%{search_keyword}%", f"%{search_keyword}%"])
+    if only_no_exper:
+        vip_sql = vip_sql + " and j.no_exper = 1"
+    vip_sql += f"""
+        group by j.id, r.name, c.name, j.job_title, j.response_pr, j.salary_type, j.salary_min, j.salary_max, j.link
+        order by j.created_at desc
+        limit 300;
+    """
+    vip_df = load_data(vip_sql, tuple(vip_params))
+    if not vip_df.empty:
+        st.write(f'共 **{len(vip_df)}** 筆結果(最多顯示100筆)')
+        st.dataframe(
+            vip_df,
+            hide_index=True,
+            use_container_width=True,
+            column_config={
+                '職缺連結': st.column_config.LinkColumn('投遞連結', display_text='前往應徵'),
+                '最低薪資': st.column_config.NumberColumn(format="%d"),
+                "最高薪資": st.column_config.NumberColumn(format="%d"),
+                'hr回覆率': st.column_config.NumberColumn('回覆率', format='%d%%')
+            }
+        )
+    else:
+        st.info(f"請放寬搜尋標準")
